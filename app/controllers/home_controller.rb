dbf3c6b28be9bdb6d4c6a27bf3eca99a650cd6ef
justi <justine84@gmail.com>
2011-09-09 15:38:11 +0200

    admin panel: user listing can be sorted by id and email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,21 @@
 class HomeController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
   end
 
   def admin_panel_index
-    @users = User.order(:email)
+    @users = User.order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
